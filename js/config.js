b1fad17c11661e7f90351a5e594546df54a77c86
eros <eroskoller@gmail.com>
2016-12-05 10:12:56 -0200

    tm_site configuracao

--- js/config.js
+++ js/config.js
@@ -44,7 +44,28 @@ console.log('construing config , registrando state');
             .state('geslab', {
                 url: "/geslab",
                 templateUrl: "views/geslab/geslab_site.html",
-                data: {pageTitle: 'Landing page', specialClass: 'landing-page'},
+                data: {pageTitle: 'GesLab', specialClass: 'landing-page'},
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load([
+                            {
+                                files: ['js/plugins/wow/wow.min.js']
+                            },
+                            {
+                                files: ['css/plugins/slick/slick.css', 'css/plugins/slick/slick-theme.css', 'js/plugins/slick/slick.min.js']
+                            },
+                            {
+                                name: 'slick',
+                                files: ['js/plugins/slick/angular-slick.min.js']
+                            }
+                        ]);
+                    }
+                }
+            })
+            .state('tmsite', {
+                url: "/tmsite",
+                templateUrl: "views/tmsite/tm_site.html",
+                data: {pageTitle: 'TMInformática', specialClass: 'landing-page'},
                 resolve: {
                     loadPlugin: function ($ocLazyLoad) {
                         return $ocLazyLoad.load([
