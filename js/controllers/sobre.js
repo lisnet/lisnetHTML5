7f591b55c9129ad071f0082e0a827994bb54061a
eros <eroskoller@gmail.com>
2016-12-14 11:09:21 -0200

    tela sobre Thiago

--- js/controllers/sobre.js
+++ js/controllers/sobre.js
@@ -8,7 +8,19 @@
 function sobre($scope,sairDoSistemaService){
     
     $scope.userDTO = sairDoSistemaService.validarLogin();
+    $scope.varSimple ="test Thiago";
+    $scope.arraySimple = ['test array','second test Array',2,3,new Date()];
+    $scope.simpleJSON = {firstVar: 'Sei Lah oque', secondVar:'Nunsei di Nada', simpleDate:new Date()};
+    $scope.typeOfArray  = typeof $scope.arraySimple;
     
+    /**
+     * 
+     * @param {type} position
+     * @returns {Number|String|Date}
+     */
+    $scope.buscaPosicaoArray = function (position){
+        return $scope.arraySimple[position];
+    };
     
 }
 
